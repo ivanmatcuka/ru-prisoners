79a194a315847afacf0246389fbd2e33ce20a6b6
Ivan Matcuka <ivanmatcuka@protonmail.com>
2024-01-18 20:50:17 +0100

    chore: title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
         />
-        <title>Концерт солидарности</title>
+        <title>benefizkonzert</title>
       </Head>
       <Component {...pageProps} />
     </>
